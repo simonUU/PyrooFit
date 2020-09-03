4de090f315730edc0d0f53c9f412d61ec78d8a47
Simon Wehle <simon.wehle@desy.de>
2020-09-03 10:00:02 +0200

    minor chanve

--- examples/prod_pdf.py
+++ examples/prod_pdf.py
@@ -14,9 +14,9 @@ x = np.random.multivariate_normal(mean, cov, 300)
 df = pd.DataFrame({'x': x[:, 0], 'y': x[:, 1]})
 
 # Initialize the PDF
-pdf1 = Gauss(['x', -1, 5], mean=[0, 3], sigma=[0.1, 2], name='gx')
-pdf2 = Gauss(['y', -1, 5], mean=[0, 3], sigma=[0.1, 2], name='gy')
-pdf = pdf1*pdf2
+pdf_x = Gauss(['x', -1, 5], mean=[0, 3], sigma=[0.1, 2], name='gx')
+pdf_y = Gauss(['y', -1, 5], mean=[0, 3], sigma=pdf_x.parameters.sigma, name='gy')
+pdf = pdf_x * pdf_y
 
 # Fit
 pdf.fit(df)
